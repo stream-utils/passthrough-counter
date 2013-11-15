1a7b94179ac578e4cf2ca34684e7acfab3f064a5
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-15 11:40:35 -0800

    initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var path = require('path')
+var assert = require('assert')
+
+var Counter = require('./')
+
+var pack = path.join(__dirname, 'package.json')
+var length = fs.statSync(pack).size
+
+describe('Passthrough Counter', function () {
+  it('should work', function (done) {
+    var stream = fs.createReadStream(pack)
+    var counter = Counter()
+    stream.pipe(counter)
+    .once('finish', function () {
+      assert.equal(this.length, length)
+      done()
+    })
+    .resume()
+  })
+})
